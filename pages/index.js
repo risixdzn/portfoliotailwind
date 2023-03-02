50834b79bc6e145382d3cbc33b8a9f2c043148f1
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-03-01 22:35:31 -0300

    hero

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillTwitterCircle , AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
+
+import Image from 'next/image'
+import deved from '../public/dev-ed-wave.png'
 
 export default function Home() {
   return (
@@ -15,9 +19,22 @@ export default function Home() {
             <h1 className='text-xl'>Ricardo Amorim</h1>
             <ul className='flex items-center'>
               <li className='cursor-pointer text-2xl'><BsFillMoonStarsFill/></li>
-              <li><a href='#' className='bg-cyan-500 text-white px-4 py-2 rounded-md ml-8'>Resume</a></li>
+              <li><a href='#' className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Resume</a></li>
             </ul>
           </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium'>Ricardo Amorim</h2>
+            <h3 className='text-2xl py-2'>Developer and designer.</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>A student learning front-end development y himself. Join me down below and let's work together!</p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillTwitterCircle/>
+            <AiFillLinkedin/>
+            <AiFillYoutube/>
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <Image src={deved} layout='fill' objectFit='cover'/>
+          </div>
         </section>
       </main>
     </div>
